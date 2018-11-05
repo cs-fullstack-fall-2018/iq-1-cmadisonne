857b88361008a4fbf1d46cd092ca4a1cc9b45573
Lotus <cmadisonne@gmail.com>
2018-11-05 16:43:50 -0600

    QOD

---
+++ IQ1.js
@@ -0,0 +1,12 @@
+var userInput = prompt("Who's the most awesome person you'll ever meet?");
+
+function main() {
+    if (userInput === 'maddi'){
+        document.write(userInput)
+    }
+    else {
+        document.write('Wrong! Try again');
+    }
+
+}
+ main();
